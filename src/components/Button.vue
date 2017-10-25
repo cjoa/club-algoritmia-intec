50b022e1ec75442c42f7310872ae28b6ccf8d7b6
evalir.io <hi.evalir@gmail.com>
2017-10-25 13:51:01 -0400

    Added dummy landing page

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="Button button-dark">
+    <slot>Button</slot>
+  </button>
+</template>
+
+
+<style scoped>
+  .button-dark {
+    width: 100px;
+    height: 50px;
+    background-color: indigo;
+    color: white;
+    border: 0 solid black;
+    border-radius: 2px;
+  }
+</style>
