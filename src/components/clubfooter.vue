d85c65b71f44f2a2e3f83e45ffbd71fb1becb78d
Roniel Valdez <roniel_valdez@outlook.com>
2017-10-25 19:06:36 -0400

    Added empty component files with a guideline on how  to make each

---
+++ src/components/clubfooter.vue
@@ -0,0 +1,7 @@
+<!-- * Normal website footer as a component, added the prefix "club" to
+     * avoid name collisions. Includes a quick list of the navbar links
+     * and commom copyright information.
+     * -->
+
+<!-- * TODO: Complete the segment component as the description says,
+     * or add what you want and see fit to the component. -->
---
+++ src/components/codeforce.vue
@@ -0,0 +1,8 @@
+<!-- * Show list of the codeforce progress of each of the members of the
+     * club, in descending order (toribio should be put separarate :) ).
+     * and the last "member" could be a picture representing a person
+     * (maybe a gray body with an interrogation mark on it) saying "you"
+     * inviting the reader to join the club-->
+
+<!-- * TODO: Complete the codeforce component as the description says,
+     * or add what you want and see fit to the component. -->
