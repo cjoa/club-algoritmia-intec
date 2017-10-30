7f87d3c88428c6dcee0a469e0617c415084f72c5
Roniel Valdez <roniel_valdez@outlook.com>
2017-10-30 12:41:50 -0400

    Added simple navigation bar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// jQuery as required by Bootstrap JS
+import jQuery from 'jquery'
+global.jQuery = jQuery  
+let Bootstrap = require('bootstrap-sass')
+// Adding the CSS file manually as webpack is not loading 
+// the CSS from the require call
 require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/Landing'
+import App from '../App'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Landing',
-      component: LandingPage
+      name: 'App',
+      // components: 
     }
   ]
 })
